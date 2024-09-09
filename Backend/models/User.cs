88d41c56a162295a819cdd6b508141665d5e5d46
calle1212 <ch98@hotmail.se>
2024-09-09 21:07:05 +0200

    add ArchiveActivePost and SetActivePost methods

--- Backend/models/User.cs
+++ Backend/models/User.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Backend.models
+namespace Backend.models;
+
+public class User
 {
-    public class User
+    public int Id;
+    required public string ClerkId { get; init; }
+    required public string Name { get; set; }
+    public List<Review> Reviews { get; } = [];
+    public Post? ActivePost { get; private set; }
+    public List<Post> PostHistory { get; } = [];
+
+
+    public bool ArchiveActivePost()
     {
-        public int Id;
-        required public string ClerkId {get; init;} 
-        required public string Name { get; set; }
-        public List<Review> Reviews { get; } = [];
-        public Post? ActivePost {get; set;}
-        public List<Post> PostHistory { get; } = [];
+        if (ActivePost is not null)
+        {
+            PostHistory.Add(ActivePost);
+            this.ActivePost = null;
+            return true;
+        }
+        return false;
+    }
+    public bool SetActivePost(Post post)
+    {
+        if (ActivePost is null)
+        {
+            ActivePost = post;
+            return true;
+        }
+        return false;
     }
 }
+
+
