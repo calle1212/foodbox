339c07aa1ffb402bc8d45bd466954cd156a88ef9
calle1212 <ch98@hotmail.se>
2024-09-09 21:20:31 +0200

    feat: add AddFulfiller method

--- Backend/models/Post.cs
+++ Backend/models/Post.cs
@@ -1,18 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Backend.models
+namespace Backend.models;
+
+public class Post
 {
-    public class Post
+    required public string Title { get; set; }
+    required public string Description { get; set; }
+    public int Payment { get; set; }
+    required public string Location { get; set; }
+    public DateTime Date;
+    required public User Creator { get; init; }
+    public User? Fulfiller { get; private set; }
+
+    public bool AddFulfiller(User fulfiller)
     {
-        required public string Title { get; set; }
-        required  public string Description { get; set; }
-        public int Payment { get; set; }
-        required public string Location { get; set; }
-        public DateTime Date;
-        required public User Creator { get; init; }
-        public User? Fulfiller { get; set; }
+        if(Fulfiller is null ) 
+        {
+            Fulfiller = fulfiller;
+            return true;
+        }
+        return false;
     }
+
 }
+
+
