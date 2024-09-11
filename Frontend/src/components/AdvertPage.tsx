6fc936b42f236d00d285d7a88cff7bbf72a86edd
calle1212 <ch98@hotmail.se>
2024-09-11 22:01:40 +0200

    feat: add to deal page

--- Frontend/src/components/AdvertPage.tsx
+++ Frontend/src/components/AdvertPage.tsx
@@ -10,11 +10,24 @@ export default function AdvertPage(){
     const queryClient = useQueryClient();
 
     const data : Post[] = queryClient.getQueryData(["repoData"])
-    const thisPost = data.find(p => p.id == qid);
+    const Post = data.find(p => p.id == qid);
 
     console.log(data);
-    console.log(thisPost);
+    console.log(Post);
     return (
-        <p>hej</p>
+        <div className='flex flex-col items-center'>
+            <h1>{Post?.title}</h1>
+            <figure className='max-w-96'>
+                    <img
+                        src={Post?.imageUrl || "https://images.pexels.com/photos/1860208/pexels-photo-1860208.jpeg?cs=srgb&dl=cooked-food-1860208.jpg&fm=jpg"}
+                        alt="Food image" />
+                </figure>
+            <p>{Post?.description}</p>
+            <p>{Post?.date}</p>
+            <p>{Post?.location}</p>
+            <p>{Post?.payment} kr</p>
+
+            <button className='btn btn-primary'>Take Job</button>
+        </div>
     )
 }
