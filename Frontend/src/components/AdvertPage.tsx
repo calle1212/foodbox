88d13a0fb61fbda4bc81fbcaa56e5986aaebafa2
calle1212 <ch98@hotmail.se>
2024-09-11 21:39:18 +0200

    access data from deal page

--- Frontend/src/components/AdvertPage.tsx
+++ Frontend/src/components/AdvertPage.tsx
@@ -1,8 +1,19 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { Post } from '../types';
+import { useMatch } from '@tanstack/react-router';
 
 export default function AdvertPage(){
+    const { search } = useMatch("/deal");
+    const qid= search.id;
+    console.log(qid);
 
-    // Access the state passed from HomePage
+    const queryClient = useQueryClient();
+
+    const data : Post[] = queryClient.getQueryData(["repoData"])
+    const thisPost = data.find(p => p.id == qid);
+
+    console.log(data);
+    console.log(thisPost);
     return (
         <p>hej</p>
     )
